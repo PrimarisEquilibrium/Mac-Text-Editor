2e7c2363d3698a5b5442a078671749836bad537f
PrimarisEquilibrium <incorrectclock1@gmail.com>
2025-04-15 09:35:40 -0400

    feat: initial commit

---
+++ Mac Text Editor/Mac_Text_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mac_Text_EditorApp.swift
+//  Mac Text Editor
+//
+//  Created by Kevin El-Khoury on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct Mac_Text_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mac Text EditorTests/Mac_Text_EditorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Mac_Text_EditorTests.swift
+//  Mac Text EditorTests
+//
+//  Created by Kevin El-Khoury on 2025-04-15.
+//
+
+import Testing
+
+struct Mac_Text_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
